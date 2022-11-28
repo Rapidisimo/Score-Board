49faacf2af4d421844692413e6c60c3b752b0efb
Josué Rodriguez <13837690+Rapidisimo@users.noreply.github.com>
2022-11-28 14:51:57 -0500

    functionality complete

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Scrimba Solo Project - Score Board
 
 ## Requirements
-1. Build it from "scratch"
-2. Follow the figma design
-3. Make all six buttons work
+1. ✅ Build it from "scratch"
+2. ✅ Follow the figma design
+3. ✅ Make all six buttons work
 
 ## Stretch Goals - Not Required
-1. Add a "New Game" button
-2. Highlight the leader
+1. ✅ Add a "New Game" button
+2. ✅ Highlight the leader
 3. Add a few more counters (e.g. period, fouls, timer)
 4. Change the design
--- index.js
+++ index.js
@@ -1,40 +1,63 @@
-const homeScore = document.getElementById('home-score');
-const guestScore = document.getElementById('guest-score');
-const scoreButton = document.querySelector('.board');
-const newGame = document.getElementById('newgame');
+const homeScore = document.getElementById('home-score'); //Home Points
+const guestScore = document.getElementById('guest-score'); //Guest Points
+const scoreButton = document.querySelector('.board'); //All +Score Buttons
+const newGame = document.getElementById('newgame'); //New Game Button
 
+// Initial Score
 let homePoints = 0;
 let guestPoints = 0;
 
-
-
+// Event listener for all buttons that checks what points to assign, team it's for and highlights leader
 scoreButton.addEventListener('click', e => {
-    if (e.target.textContent === '+1' && e.target.parentElement.classList == 'home') {
+    if (e.target.textContent === '+1' && e.target.parentElement.classList.contains('home')) {
         homePoints += 1;
         homeScore.textContent = homePoints;
-        console.log(e.target.parentElement.classList == 'home')
-    } else if (e.target.textContent === '+2' && e.target.parentElement.classList == 'home') {
+        leader();
+    } else if (e.target.textContent === '+2' && e.target.parentElement.classList.contains('home')) {
         homePoints += 2;
         homeScore.textContent = homePoints;
-    } else if (e.target.textContent === '+3' && e.target.parentElement.classList == 'home') {
+        leader();
+    } else if (e.target.textContent === '+3' && e.target.parentElement.classList.contains('home')) {
         homePoints += 3;
         homeScore.textContent = homePoints;
-    } else if (e.target.textContent === '+1' && e.target.parentElement.classList == 'guest') {
+        leader();
+    } else if (e.target.textContent === '+1' && e.target.parentElement.classList.contains('guest')) {
         guestPoints += 1;
         guestScore.textContent = guestPoints;
-    } else if (e.target.textContent === '+2' && e.target.parentElement.classList == 'guest') {
+        leader();
+    } else if (e.target.textContent === '+2' && e.target.parentElement.classList.contains('guest')) {
         guestPoints += 2;
         guestScore.textContent = guestPoints;
-    } else if (e.target.textContent === '+3' && e.target.parentElement.classList == 'guest') {
+        leader();
+    } else if (e.target.textContent === '+3' && e.target.parentElement.classList.contains('guest')) {
         guestPoints += 3;
         guestScore.textContent = guestPoints;
+        leader();
     }
 });
 
-
+// Resets the game score tracking.
 newGame.addEventListener('click', () => {
     homePoints = 0;
     guestPoints = 0;
     homeScore.textContent = homePoints;
     guestScore.textContent = guestPoints;
+    leader();
 });
+
+// Checks and hightlights which teams has the highest score.
+function leader () {
+    let home = document.querySelector('.home');
+    let guest = document.querySelector('.guest');
+
+    if (homePoints > guestPoints) {
+        guest.classList.remove('leader');
+        home.classList.add('leader');
+    } else if (homePoints < guestPoints) {
+        home.classList.remove('leader');
+        guest.classList.add('leader');
+    } else if ( homePoints === guestPoints ) {
+        home.classList.remove('leader');
+        guest.classList.remove('leader');
+    }
+}
